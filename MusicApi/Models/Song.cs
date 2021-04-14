b4cdd76f7d11ba9478ebe6bb2fa93ef57acedc5f
Lukasz Piasecki <KWHQ38@motorolasolutions.com>
2021-04-14 12:36:07 +0200

    Create Song model

---
+++ MusicApi/Models/Song.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicApi.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Length { get; set; }
+    }
+}
