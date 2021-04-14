4c8f02f135203bad9fcea588128a116453017ccd
Lukasz Piasecki <KWHQ38@motorolasolutions.com>
2021-04-14 12:41:09 +0200

    Setup in memory database connection

---
+++ MusicApi/Data/MusicApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicApi.Models;
+
+namespace MusicApi.Data
+{
+    public class MusicApiContext : DbContext
+    {
+        public MusicApiContext(DbContextOptions<MusicApiContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Song> Songs { get; set; }
+    }
+}
--- MusicApi/Startup.cs
+++ MusicApi/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MusicApi.Data;
 
 namespace MusicApi
 {
@@ -27,6 +29,8 @@ namespace MusicApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<MusicApiContext>(opt =>
+                opt.UseInMemoryDatabase("MusicApi"));
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "MusicApi", Version = "v1"}); });
         }
 
